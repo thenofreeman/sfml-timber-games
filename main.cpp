4bdd046ac2185d20001141c480ab5d7c0f9c0cb3
nolan freeman abbott <nolan@freemanabbott.com>
2023-05-16 12:11:34 -0500

    add new textures to view and adjust code ws

--- main.cpp
+++ main.cpp
@@ -3,15 +3,48 @@
 
 int main(/*int argc, char *argv[]*/)
 {
-    sf::RenderWindow window (sf::VideoMode(1920,1080), "SFML Works!");
+    sf::RenderWindow window (sf::VideoMode(1920, 1080), "SFML Works!");
 
     sf::Texture textureBackground;
     textureBackground.loadFromFile("graphics/background.png");
-
     sf::Sprite spriteBackground;
     spriteBackground.setTexture(textureBackground);
 
-    spriteBackground.setPosition(0,0);
+    spriteBackground.setPosition(0, 0);
+
+    sf::Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+    sf::Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810, 0);
+
+    sf::Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    sf::Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0, 800);
+
+    bool beeActive = false;
+    float beeSpeed = 0.0f;
+
+    sf::Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+    sf::Sprite spriteCloud1;
+    sf::Sprite spriteCloud2;
+    sf::Sprite spriteCloud3;
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+    spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 250);
+    spriteCloud3.setPosition(0, 500);
+
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
 
     while (window.isOpen())
     {
@@ -23,7 +56,15 @@ int main(/*int argc, char *argv[]*/)
         }
 
         window.clear();
+
         window.draw(spriteBackground);
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+
+        window.draw(spriteTree);
+        window.draw(spriteBee);
+
         window.display();
     }
 
