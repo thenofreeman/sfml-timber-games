e63bb39f1350caa7d70bc393e5ec8f0f836a1eda
nolan freeman abbott <nolan@freemanabbott.com>
2023-05-16 18:41:25 -0500

    animate punching log sprites off screen

--- main.cpp
+++ main.cpp
@@ -304,6 +304,19 @@ int main(/*int argc, char *argv[]*/)
                     branches[i].setPosition(3000, height);
                 }
             }
+
+            if (logActive)
+            {
+                spriteLog.setPosition(
+                    spriteLog.getPosition().x + (logSpeedX * dt.asSeconds()),
+                    spriteLog.getPosition().y + (logSpeedY * dt.asSeconds())
+                                      );
+                if (spriteLog.getPosition().x < -100 || spriteLog.getPosition().x > 2000)
+                {
+                    logActive = false;
+                    spriteLog.setPosition(810, 720);
+                }
+            }
         }
 
         window.clear();
