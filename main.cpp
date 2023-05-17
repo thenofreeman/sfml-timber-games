7c67a58699f19aa1b05d370595ccdb3bc5ad228d
nolan freeman abbott <nolan@freemanabbott.com>
2023-05-17 15:21:59 -0500

    add bat or top collision detection

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <SFML/Graphics.hpp>
 
 #include "Bat.h"
+#include "Ball.h"
 
 int main()
 {
@@ -13,7 +14,8 @@ int main()
     int score = 0;
     int lives = 3;
 
-    Bat bat (1920 / 2, 1080 - 20);
+    Bat bat (1920/2, 1080 - 20);
+    Ball ball (1920/2, 0);
 
     sf::Text hud;
 
@@ -31,9 +33,34 @@ int main()
         sf::Time dt = clock.restart();
 
         bat.update(dt);
+        ball.update(dt);
+
         std::stringstream ss;
         ss << "Score: " << score << " Lives: " << lives;
         hud.setString(ss.str());
+
+        if (ball.getPosition().top > window.getSize().y)
+        {
+            ball.reboundBottom();
+            lives--;
+            if (lives < 1)
+            {
+                score = 0;
+                lives = 3;
+            }
+        }
+
+        if (ball.getPosition().top < 0)
+        {
+            ball.reboundBatOrTop();
+            score++;
+        }
+
+        if (ball.getPosition().left < 0 ||
+            (ball.getPosition().left + ball.getPosition().width) > window.getSize().x)
+        {
+            ball.reboundBatOrTop();
+        }
 
         sf::Event event;
         while (window.pollEvent(event))
@@ -55,6 +82,7 @@ int main()
         window.clear();
         window.draw(hud);
         window.draw(bat.getShape());
+        window.draw(ball.getShape());
         window.display();
     }
 
